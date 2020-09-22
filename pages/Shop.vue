dde7e0732199d1bfbdd53fa346a09c7294bf06f7
ukonw303 <gameger005@gmail.com>
2020-09-22 11:59:34 +0700

    a

---
+++ pages/Shop.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Shop</h1>
+    <ul>
+      <li v-for="(i,index) in 10" :key="index">
+        สินค้า {{ i }}
+        รูปภาพ
+        ราคา ส่วนลด
+        <nuxt-link
+          :to="{ name: 'product-id',
+                 params: { id: i } }"
+                    >ปุ่มรายละเอียด</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
---
+++ pages/product/_id.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>สินค้าหมายเลข {{this.$route.params.id}}</h1>
+    <h2>รายละเอียด........</h2>
+    <h2>รูป.....</h2>
+    <h2>ราคา.......</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
